b4a5f54da1bf2cf7dc1ef9d53dad4ef0e9fdd9a6
Mega067L <angeljuares067@gmail.com>
2024-04-13 21:25:30 -0600

    memorama

---
+++ juegos/memorama/script.js
@@ -0,0 +1,91 @@
+const totalCards = 16; 
+const availableCardNames = ['ballena', 'cangrejo', 'cereza', 'foca', 
+  'limon', 'manzana', 'medusa', 'naranja', 
+  'pera', 'pez', 'platano', 'pulpo',
+  'rana', 'sandia', 'tortuga', 'uvas'];
+const imgPath = '/img/memoramas_img/';
+
+let cards = []; 
+let selectedCards = [];
+let valuesUsed = []; 
+let currentMove = 0; 
+let currentAttempts = 0;
+
+let cardTemplate = '<div class="card"><div class="back"></div><div class="face"><img src="" alt=""></div></div>';
+
+function activate(e) {
+  if (currentMove < 2) {
+    if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active')) {
+      e.target.classList.add('active');
+      selectedCards.push(e.target);
+
+      if (++currentMove == 2) {
+        currentAttempts++;
+        document.querySelector('#stats').innerHTML = currentAttempts + ' intentos';
+
+        if (selectedCards[0].querySelectorAll('.face img')[0].alt == selectedCards[1].querySelectorAll('.face img')[0].alt) {
+          selectedCards = [];
+          currentMove = 0;
+
+          if (document.querySelectorAll('.card.active').length === totalCards) { 
+            displayCompletedMessage();
+          }
+        } else {
+          setTimeout(() => {
+            selectedCards[0].classList.remove('active');
+            selectedCards[1].classList.remove('active');
+            selectedCards = [];
+            currentMove = 0;
+          }, 600);
+        }
+      }
+    }
+  }
+}
+
+function getFaceImage(imgName) {
+  return imgPath + imgName + '.jpg';  // Example: /img/memoramas_img/rana.jpg
+}
+
+function getMatchingEnglishImage(imgName) {
+  return imgPath + imgName + '_ingles.jpg';
+}
+
+function randomValue() {
+  let rnd = Math.floor(Math.random() * totalCards * 0.5);
+  let values = valuesUsed.filter(value => value === rnd);
+  if (values.length < 2) {
+    valuesUsed.push(rnd);
+  } else {
+    randomValue();
+  }
+}
+
+function displayCompletedMessage() {
+  const statsElement = document.querySelector('#stats');
+  statsElement.innerHTML = '¡Completado!';
+
+  setTimeout(() => {
+    window.location.href = '/index.html'; // Redirect after 10 seconds
+  }, 5000); // 10000 milliseconds = 10 seconds
+}
+
+// ... [HTML structure] ...
+
+for (let i=0; i < totalCards; i++) {
+  let div = document.createElement('div');
+  div.innerHTML = cardTemplate;
+  cards.push(div);
+  document.querySelector('#game').append(cards[i]);
+
+  randomValue(); // Get a random card name
+
+  const cardImage = getFaceImage(availableCardNames[valuesUsed[i]]);
+  const matchingEnglishImage = getMatchingEnglishImage(availableCardNames[valuesUsed[i]]);
+
+  // Initially set the matching English image (hidden)
+  cards[i].querySelectorAll('.face img')[0].src = matchingEnglishImage; 
+  cards[i].querySelectorAll('.face img')[0].alt = availableCardNames[valuesUsed[i]];
+
+  cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
+}
