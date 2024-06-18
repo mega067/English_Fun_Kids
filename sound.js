82dfb673ac03f86b07de523b37a9607a117dae3d
Mega067L <angeljuares067@gmail.com>
2024-06-17 20:54:39 -0600

    arreglo sonido ahora espera al terminar para redirijir

--- sound.js
+++ sound.js
@@ -1,11 +1,17 @@
-// script.js
 document.addEventListener('DOMContentLoaded', (event) => {
     const links = document.querySelectorAll('a');
     const clickSound = new Audio('/sound/click.mp3'); // Ruta a tu archivo de sonido
 
     links.forEach(link => {
         link.addEventListener('click', (e) => {
+            e.preventDefault(); // Previene la redirección inmediata
+            const url = link.href; // Guarda la URL del enlace
             clickSound.play();
+
+            // Espera a que el sonido termine de reproducirse antes de redirigir
+            clickSound.addEventListener('ended', () => {
+                window.location.href = url;
+            });
         });
     });
 });
